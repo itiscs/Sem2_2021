4161d1cf7dd7d0e92ccc767556dd65563b67002d
moudroff <moudroff>
2021-02-24 13:35:57 +0300

    MyStack и MyQueue

---
+++ StackApp/StackApp/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackApp
+{
+    class Program
+    {
+        static void SafePop(MyQueue st)
+        {
+            if (!st.IsEmpty())
+                Console.WriteLine(st.Pop());
+            else
+                Console.WriteLine("Queue is empty");           
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            
+            //var st = new MyStack();
+
+            //st.Push(5);
+            //st.Push(2);
+            //st.Push(3);
+            //st.Push(6);
+            //if(!st.IsEmpty())
+            //    Console.WriteLine(st.Pop());
+            //st.Push(15);
+            //st.Push(25);
+
+
+            //Console.WriteLine(st);
+
+            //SafePop(st);
+            //SafePop(st); 
+            //SafePop(st); 
+            //SafePop(st);
+            //SafePop(st);
+            //SafePop(st);
+            //SafePop(st);
+            //SafePop(st);
+            //SafePop(st);
+
+
+            var st = new MyQueue();
+
+            st.Push(5);
+            st.Push(2);
+            st.Push(3);
+            st.Push(6);
+            if (!st.IsEmpty())
+                Console.WriteLine(st.Pop());
+            st.Push(15);
+            st.Push(25);
+
+
+            Console.WriteLine(st);
+
+            SafePop(st);
+            SafePop(st);
+            SafePop(st);
+            SafePop(st);
+            SafePop(st);
+            SafePop(st);
+            SafePop(st);
+            SafePop(st);
+            SafePop(st);
+
+
+
+
+        }
+    }
+}
---
+++ StackApp/StackApp/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("StackApp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StackApp")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("98ef51d8-024f-4c0b-8e4f-3cb61f571473")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
