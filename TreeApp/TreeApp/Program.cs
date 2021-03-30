098b29f05c0b2b09dcf97058013db5dc5f82be14
moudroff <moudroff>
2021-03-30 17:01:20 +0300

    Create table from string

--- TreeApp/TreeApp/Program.cs
+++ TreeApp/TreeApp/Program.cs
@@ -37,6 +37,29 @@ namespace TreeApp
             return result;
         }
 
+        public int Calc()
+        {
+            if (Left == Right)
+                return int.Parse(Info.ToString());
+
+            var str = Info.ToString();
+            if (str[0] == '(')
+                str = str.Remove(0, 1);
+            switch (str)
+            {
+                case "+":
+                    return Left.Calc() + Right.Calc();
+                case "-":
+                    return Left.Calc() - Right.Calc();
+                case "*":
+                    return Left.Calc() * Right.Calc();
+                case "/":
+                    return Left.Calc() / Right.Calc();
+            }
+            return 0;
+
+        }
+
 
 
     }
@@ -57,6 +80,49 @@ namespace TreeApp
             return init;
         }
 
+        public Tree()
+        {
+
+        }
+        public Tree(string str, Func<string, T> parser)
+        {
+            // (1,(2,4,(5,8,)),(3,6,7))
+            // (*,(+,4,(/,8,4)),(-,6,7))
+            // (1 (2 4 (5 8 _ _ _ _ (3 6 7 _
+            int k = 0;
+            Root = Create(str.Split(new char[] { ',', ')' }), ref k, parser);
+
+        }
+
+        public Elem<T> Create(String[] str, ref int k, Func<string, T> parser)
+        {
+            Elem<T> t = new Elem<T>();
+            if (!str[k].Contains('('))
+            {
+                t.Info = parser(str[k]);
+            }
+            else
+            {
+                t.Info = parser(str[k].Remove(0, 1));//убрали '(' 
+                k++;
+                if (str[k] != "")
+                    t.Left = Create(str, ref k, parser);
+                k++;
+                if (str[k] != "")
+                    t.Right = Create(str, ref k, parser);
+                k++;
+            }
+            return t;
+        }
+
+
+        public int Calc()
+        {
+            return Root.Calc();
+        }
+
+
+
     }
 
 
@@ -65,25 +131,35 @@ namespace TreeApp
     {
         static void Main(string[] args)
         {
+            //   Tree<int> tree = new Tree<int>("(100,4,)", s => int.Parse(s));
 
-            Tree<int> tree = new Tree<int>();
-            tree.Root = new Elem<int>()
-            {
-                Info = 5,
-                Left = new Elem<int>() 
-                { 
-                    Info = 4,
-                    Left = new Elem<int>() { Info = 10 },
-                    Right = new Elem<int>() { Info = 8 }
-                },
-                Right = new Elem<int>()
-                {
-                    Info = 3,
-                    Left = new Elem<int>() { Info = 66 }
-                }
-            };
+            Tree<int> tree = new Tree<int>("(-10,(23,46,(5,8,)),(13,36,7))", s => int.Parse(s) );
 
-            Console.WriteLine(tree);
+            Tree<string> arTree = new Tree<string>("(/,(*,(+,3,13), 7),6)", s => s);
+
+            //Tree<string> arTree = new Tree<string>("(*, (+, 4, (/, 8, 4)), (-, 6, 7))", s => s);
+           // Tree<double> tree = new Tree<double>("(-10,4;(23,4;46,3;(5,2;8;));(13;36;7))", s => double.Parse(s));
+
+            //tree.Root = new Elem<int>()
+            //{
+            //    Info = 5,
+            //    Left = new Elem<int>() 
+            //    { 
+            //        Info = 4,
+            //        Left = new Elem<int>() { Info = 10 },
+            //        Right = new Elem<int>() { Info = 8 }
+            //    },
+            //    Right = new Elem<int>()
+            //    {
+            //        Info = 3,
+            //        Left = new Elem<int>() { Info = 66 }
+            //    }
+            //};
+
+
+
+            Console.WriteLine(arTree);
+            Console.WriteLine(arTree.Calc());
 
             Console.WriteLine(tree.MyOper((a, b) => a + b, 0));
 
